793334e51e6d1de2d0e728f2fcb033c79d2da9c2
SyedHabiburRaheman <syedhabiburraheman@gmail.com>
2021-01-16 18:07:50 +0530

    Media Queries Added

--- changeScript.js
+++ changeScript.js
@@ -1,9 +1,9 @@
 function pictureChange() {
-    var image = document.getElementById('img');
-    if (image.src.match("/Assets/images/computer2.jpg")) {
-        image.src = "/Assets/images/computer2b.jpg";
+    var image = document.getElementById("img");
+    if (image.src.match("Assets/images/computer2.jpg")) {
+        image.src = "Assets/images/computer2b.jpg";
     }
     else {
-        image.src = "/Assets/images/computer2.jpg";
+        image.src = "Assets/images/computer2.jpg";
     }
 }
